f5b88a22bcfd65144606d901204ac40aaa85ca19
David-Koshkadze <rootUS3rr@pm.me>
2023-05-30 20:51:56 +0400

    fix quill options

--- markup-task/src/App.jsx
+++ markup-task/src/App.jsx
@@ -12,7 +12,10 @@ import "react-quill/dist/quill.snow.css";
 const modules = {
   toolbar: [
     ["bold", "italic", "underline"],
-    [{ 'list': 'ordered'}, { 'list': 'bullet' }],
+    [{ list: "ordered" }, { list: "bullet" }],
+    [{ header: 1 }, { header: 2 }],
+    ["link"],
+    ["clean"],
   ],
 };
 
@@ -62,6 +65,7 @@ function App() {
             theme="snow"
             placeholder="Type or paste (⌘+V) something here."
           />
+          <div className="editor__count__box">12 Characters, 3 words</div>
         </div>
       </section>
     </>
