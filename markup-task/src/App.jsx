8d19b58c05b48588ad07a93b2bd77ce815a4be29
David-Koshkadze <rootUS3rr@pm.me>
2023-05-30 11:06:43 +0400

    add buttons & header styles

--- markup-task/src/App.jsx
+++ markup-task/src/App.jsx
@@ -1,12 +1,34 @@
-import './App.css'
+import "./App.css";
 
 function App() {
-
   return (
     <>
-    <h1>Hello World</h1>
+      <header>
+        <nav className="navbar">
+          <div style={{ display: "flex", alignItems: "center", gap: "1rem" }}>
+            <button
+              className="btn btn-secondary"
+              style={{ display: "flex", alignItems: "center", gap: "0.6rem" }}
+            >
+              <img src="icons/icon-chrome.svg" alt="icon-chrome" />
+              <span>Add to Chrome</span>
+            </button>
+            <button className="btn btn-primary">Upgrade to Pro</button>
+          </div>
+          {/* Dropdown */}
+          <div className="avatar-dropdown"></div>
+        </nav>
+      </header>
+
+      <div className="sidebar">
+        <div>Hello</div>
+      </div>
+
+      <section className="section-container">
+        <button className="btn btn-primary">Check</button>
+      </section>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
